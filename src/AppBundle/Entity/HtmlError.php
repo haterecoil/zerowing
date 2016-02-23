eb02d0cd39cb55c2a2daddcb14f040f00b2c672e
MilenaTT <milena.tarriere@gmail.com>
2016-02-23 21:08:59 +0100

    html_error entity + update goslingResponse

---
+++ src/AppBundle/Entity/HtmlError.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * HtmlError
+ *
+ * @ORM\Table(name="html_error")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\HtmlErrorRepository")
+ */
+class HtmlError
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="value", type="string", length=255)
+     */
+    private $value;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set value
+     *
+     * @param string $value
+     * @return HtmlError
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * Get value
+     *
+     * @return string 
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
