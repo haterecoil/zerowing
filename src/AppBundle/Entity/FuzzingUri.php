59497712993b9615a0ab39ed9f5af74437c92eb5
Morgan Caron <morgan.caron@hetic.net>
2016-02-23 14:14:30 +0100

    FuzzingUri Entity

---
+++ src/AppBundle/Entity/FuzzingUri.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * FuzzingUri
+ *
+ * @ORM\Table(name="fuzzing_uri")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\FuzzingUriRepository")
+ */
+class FuzzingUri
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="uri", type="string", length=255)
+     */
+    private $uri;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=255)
+     */
+    private $type;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set uri
+     *
+     * @param string $uri
+     * @return FuzzingUri
+     */
+    public function setUri($uri)
+    {
+        $this->uri = $uri;
+
+        return $this;
+    }
+
+    /**
+     * Get uri
+     *
+     * @return string 
+     */
+    public function getUri()
+    {
+        return $this->uri;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     * @return FuzzingUri
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string 
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+}
