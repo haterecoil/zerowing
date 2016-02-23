3bdd4709f39bc7dfa211e85043fd6c16d7b2e0c9
Clarisse LAWSON <clarisselawson1@gmail.com>
2016-02-23 16:45:11 +0000

    creation entité siteclient

---
+++ src/AppBundle/Entity/SiteCLient.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="site_c_lient")
+ */
+class SiteCLient
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="url", type="string", length=255, unique=true)
+     */
+    private $url;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="validation", type="integer")
+     */
+    private $validation;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     * @return SiteCLient
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string 
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Set validation
+     *
+     * @param integer $validation
+     * @return SiteCLient
+     */
+    public function setValidation($validation)
+    {
+        $this->validation = $validation;
+
+        return $this;
+    }
+
+    /**
+     * Get validation
+     *
+     * @return integer 
+     */
+    public function getValidation()
+    {
+        return $this->validation;
+    }
+}
